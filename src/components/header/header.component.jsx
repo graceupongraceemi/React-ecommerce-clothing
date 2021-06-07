10ed13bfef63cb3910323192831cb7b3a07eeb52
Emiko Esther S <emiko.esther.self@gmail.com>
2021-06-06 18:19:34 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
-import CustomButton from '../custom-button/custom-button.component';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
